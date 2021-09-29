d91c0c895ceed025f693f6d84e3f127e1abe292f
Jesus Ruiz <hesus.ruiz@gmail.com>
2021-09-29 21:04:07 +0200

    deploy

--- src/pages/scanqr.js
+++ src/pages/scanqr.js
@@ -5,6 +5,46 @@ import {log} from '../log'
 import { BrowserMultiFormatReader, NotFoundException } from '@zxing/library';
 import { AbstractPage } from './abstractpage'
 
+const backCameraKeywords = [
+    "rear",
+    "back",
+    "rück",
+    "arrière",
+    "trasera",
+    "trás",
+    "traseira",
+    "posteriore",
+    "后面",
+    "後面",
+    "背面",
+    "后置",
+    "後置",
+    "背置",
+    "задней",
+    "الخلفية",
+    "후",
+    "arka",
+    "achterzijde",
+    "หลัง",
+    "baksidan",
+    "bagside",
+    "sau",
+    "bak",
+    "tylny",
+    "takakamera",
+    "belakang",
+    "אחורית",
+    "πίσω",
+    "spate",
+    "hátsó",
+    "zadní",
+    "darrere",
+    "zadná",
+    "задня",
+    "stražnja",
+    "belakang",
+    "बैक",
+];
 
 export class ScanQrPage extends AbstractPage {
 
@@ -22,11 +62,19 @@ export class ScanQrPage extends AbstractPage {
 
     }
 
-    async enter() {
+    isBackCameraLabel(label) {
+        const lowercaseLabel = label.toLowerCase();
+        return backCameraKeywords.some((keyword) => {
+            return lowercaseLabel.includes(keyword);
+        });
+    }
+
+    async getVideoDevices() {
+        // Get the video devices
 
         if (!navigator.mediaDevices || !navigator.mediaDevices.enumerateDevices) {
-        console.log("enumerateDevices() not supported.");
-        return;
+            console.log("enumerateDevices() not supported.");
+            return;
         }
 
         this.devices = await navigator.mediaDevices.enumerateDevices()
@@ -34,24 +82,82 @@ export class ScanQrPage extends AbstractPage {
             return device.kind === "videoinput";
         });
 
+        let stream = undefined;
+        // Check if they have labels. If they don't, it means we have to request permission from the user
+        if (this.devices.length > 0) {
+            let allLabelsEmpty = this.devices.every((device) => {return device.label === ""})
+            if (allLabelsEmpty) {
+                try {
+                    stream = await navigator.mediaDevices.getUserMedia({
+                        video: true,
+                        audio: false,
+                    });
+                    // Try again to get the devices
+                    this.devices = await navigator.mediaDevices.enumerateDevices()
+                    this.devices = this.devices.filter((device) => {
+                        return device.kind === "videoinput";
+                    });
+                }
+                catch {
+                    // Ignored
+                }
+                finally {
+                    if (stream !== undefined) {
+                        stream.getVideoTracks().forEach((track) => {
+                            track.stop();
+                        });
+                    }
+                }
+    
+    
+            }
+        }
+
+        this.frontCameras = this.devices.filter((device) => {
+            return !this.isBackCameraLabel(device.label)
+        });
+        this.backCameras = this.devices.filter((device) => {
+            return this.isBackCameraLabel(device.label)
+        });
+
+    }
+
+    async getPreferredVideoDevice() {
+        await this.getVideoDevices()
+
+        if (this.backCameras.length > 0) {
+            return this.backCameras[this.backCameras.length - 1]
+        } else if (this.frontCameras.length > 0) {
+            return this.frontCameras[0]
+        } else {
+            return undefined
+        }
+
+    }
+
+    async enter() {
+
+        // Select the most appropriate camera for scanning a QR, using some heuristics
+        this.cameraQR = await this.getPreferredVideoDevice()
+
+        // let theHtml = html`
+        //     ${this.videoElem}
+        //     <ul>
+        //     ${this.devices.map((device) =>
+        //         html`<li>${device.kind}: ${device.label}</li>`
+        //     )}
+        //     </ul>
+        // `;
         let theHtml = html`
             ${this.videoElem}
-            <ul>
-            ${this.devices.map((device) =>
-                html`<li>${device.kind}: ${device.label}</li>`
-            )}
-            </ul>
         `;
 
         // Prepare the screen, waiting for the video
         this.render(theHtml)
 
-        // Select the most appropriate camera for scanning a QR, using some heuristics
-        this.cameraQR = this.devices[this.devices.length - 1].deviceId
-
         // Call the QR decoder using the video element just created
         // The decoder will choose the appropriate camera
-        this.codeReader.decodeFromVideoDevice(this.cameraQR, this.videoElem, (result, err) => {
+        this.codeReader.decodeFromVideoDevice(this.cameraQR.deviceId, this.videoElem, (result, err) => {
             if (result) {
                 // Successful decode
                 console.log("RESULT", result)
